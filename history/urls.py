b1e84fcef7066ea595954c0163ce7f6bd7c4c6bf
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-01-23 11:24:37 -0600

    changed urls to dynamic urls

--- history/urls.py
+++ history/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "history"
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:artist_id>/', views.detail, name='detail'),
